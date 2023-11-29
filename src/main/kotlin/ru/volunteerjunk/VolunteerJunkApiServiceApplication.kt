bb27de0b4a87e025e9732071bf143548b01b9406
Мухамедзянова Алиса <m.alisa.lank@gmail.com>
2023-11-29 16:54:32 +0400

    create rep

---
+++ src/main/kotlin/ru/volunteerjunk/VolunteerJunkApiServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.volunteerjunk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VolunteerJunkApiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VolunteerJunkApiServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ru/volunteerjunk/VolunteerJunkApiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.volunteerjunk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VolunteerJunkApiServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
