e2fec0a8c035ec7eebc83039b62c2f85f11459d9
Мухамедзянова Алиса <m.alisa.lank@gmail.com>
2023-12-01 00:40:48 +0400

    save v/2

--- src/main/kotlin/ru/volunteerjunk/controller/article/ArticleResponse.kt
+++
@@ -1,9 +0,0 @@
-package ru.volunteerjunk.controller.article
-
-import java.util.*
-
-data class ArticleResponse(
-        val id: UUID,
-        val title: String,
-        val content: String
-)
--- src/main/kotlin/ru/volunteerjunk/dto/Article.kt
+++
@@ -1,9 +0,0 @@
-package ru.volunteerjunk.dto
-
-import java.util.*
-
-data class Article(
-        val id: UUID,
-        val title: String,
-        val content: String
-)
